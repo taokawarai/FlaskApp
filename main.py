acb46d5d60b978fcadfecea23ff4c684952bd1bf
TAO-KAWARAI <taotako@gmail.com>
2021-12-07 06:39:22 +0900

    form入力に対応

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def hello_world():
-  return render_template("base.html")
+  if request.method == 'GET':
+    return render_template("base.html")
+  elif request.method == 'POST':
+    weight = request.form["weight"]
+    if weight:
+      water = str(float(weight) * 35)
+      return render_template('base.html', water = water)
+    return render_template("base.html")
